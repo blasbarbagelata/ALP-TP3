c074becb3d32980d561acd8a4580308a15ac363c
TomasCastro <tomascastrorojas18@gmail.com>
2021-10-31 18:59:59 -0300

    Extension en el pretty printer para numeros naturales

--- src/PrettyPrinter.hs
+++ src/PrettyPrinter.hs
@@ -43,14 +43,33 @@ pp ii vs (Let t u) =
   text "let "
     <> text (vs !! ii)
     <> text " = "
-    <> parensIf (not (isVar t)) (pp ii vs t)
+    <> parensIf (notValue t) (pp ii vs t)
     <> text " in "
     <> pp (ii + 1) vs u
+
+pp ii vs (Succ t) = 
+    text "suc "
+    <> parensIf (notValue t) (pp ii vs t)
+
+pp ii vs (Rec t1 t2 t3) = 
+    sep [text "R",
+         parensIf (notValue t1) (pp ii vs t1),
+         parensIf (notValue t2) (pp ii vs t2),
+         parensIf (notValue t3) (pp ii vs t3)]
+         
+pp ii vs Zero = text "0"
+
+isZero :: Term -> Bool
+isZero Zero = True
+isZero _    = False
 
 isVar :: Term -> Bool 
 isVar (Bound _) = True
 isVar (Free _)  = True 
 isVar _         = False 
+
+notValue :: Term -> Bool
+notValue t = not (isZero t || isVar t)
 
 isLam :: Term -> Bool
 isLam (Lam _ _) = True
@@ -67,6 +86,7 @@ isLet _         = False
 -- pretty-printer de tipos
 printType :: Type -> Doc
 printType EmptyT = text "E"
+printType NatT   = text "Nat"
 printType (FunT t1 t2) =
   sep [parensIf (isFun t1) (printType t1), text "->", printType t2]
 
@@ -80,7 +100,10 @@ fv (Bound _         ) = []
 fv (Free  (Global n)) = [n]
 fv (t   :@: u       ) = fv t ++ fv u
 fv (Lam _   u       ) = fv u
-fv (Let t u         ) = fv t ++ fv u         
+fv (Let t u         ) = fv t ++ fv u
+fv Zero               = []
+fv (Succ t          ) = fv t
+fv (Rec a b c       ) = fv a ++ fv b ++ fv c         
 ---
 printTerm :: Term -> Doc
 printTerm t = pp 0 (filter (\v -> not $ elem v (fv t)) vars) t
